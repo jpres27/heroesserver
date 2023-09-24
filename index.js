882eb14922d4590565715d1a406ae0b4a93cf46e
jpres <john.presley.27@gmail.com>
2023-09-24 11:55:59 -0400

    fixed port settings

--- index.js
+++ index.js
@@ -83,6 +83,6 @@ app.post('/api/heroes', (request, response) => {
     response.json(hero)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
